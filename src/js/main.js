40a0e59f601ece94f1a30e2e401eaef1ed15fa7a
Lareta064 <lareta064@gmail.com>
2023-03-27 11:15:13 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,43 @@
 document.addEventListener("DOMContentLoaded", function (){
 
 	/* ==============показать модальные окна,  имеют атрибут frame-modal , кнопка, которая его показывает , имеет атрибут frame-btn, Чтобы закрыть такое окно, прописываем кнопке закрытия атрибут frame-close*/
-	// const modalFramesOpen = document.querySelectorAll('[frame-btn]');
-	// const modalFrames = document.querySelectorAll('[frame-modal]');
+	const modalFramesOpen = document.querySelectorAll('[frame-btn]');
+	const modalFrames = document.querySelectorAll('[frame-modal]');
+	const bodyEl = document.body;
 
-	// if( modalFrames.length > 0){
+	if( modalFrames.length > 0){
 		
-	// 	const modalFramesClose = document.querySelectorAll('[frame-close]');
-	// 	for(let item of modalFramesOpen){
-	// 		item.addEventListener('click', function(e){
-	// 			for(let item of  modalFrames){
-	// 				item.classList.remove('visible');
-	// 				bodyEl.classList.remove('noscroll');
-	// 				overlayBg.classList.remove('active');
-					
-	// 			}
-	// 			e.preventDefault();
-	// 			const itemAttr = item.getAttribute('frame-btn');
+		const modalFramesClose = document.querySelectorAll('[frame-close]');
+		for(let item of modalFramesOpen){
+			item.addEventListener('click', function(e){
+				for(let item of  modalFrames){
+					item.classList.remove('visible');
+					bodyEl.classList.remove('lock');
+				}
+				e.preventDefault();
+				const itemAttr = item.getAttribute('frame-btn');
 
-	// 			for(let frame of modalFrames){
-	// 				const frameAttr =frame.getAttribute('frame-modal');	
-	// 				if(frameAttr == itemAttr){
-	// 					frame.classList.add('visible');
-	// 					bodyEl.classList.add('noscroll');
-	// 					overlayBg.classList.add('active');
-	// 				}
-	// 			}
-	// 		});
-	// 	}
-	// 	/*закрыть модалки с атрибутом frame-modal*/
-	// 	for(let item of modalFramesClose){
-	// 		item.addEventListener('click', function(e){
+				for(let frame of modalFrames){
+					const frameAttr =frame.getAttribute('frame-modal');	
+					if(frameAttr == itemAttr){
+						frame.classList.add('visible');
+						bodyEl.classList.add('lock');
+						
+					}
+				}
+			});
+		}
+		/*закрыть модалки с атрибутом frame-modal*/
+		for(let item of modalFramesClose){
+			item.addEventListener('click', function(e){
 				
-	// 			e.preventDefault();
-	// 			item.closest('[frame-modal]').classList.remove('visible');
-	// 			bodyEl.classList.remove('noscroll');
-	// 			overlayBg.classList.remove('active');
-	// 		});
-	// 	}
-	// }
+				e.preventDefault();
+				item.closest('[frame-modal]').classList.remove('visible');
+				bodyEl.classList.remove('lock');
+				
+			});
+		}
+	}
 	// overlayBg.addEventListener('click', function(e){
 	// for(let frame of modalFrames){
 	// 	frame.classList.remove('visible');
@@ -79,6 +78,17 @@ document.addEventListener("DOMContentLoaded", function (){
 	// 	speed:800,
 	// 	loop: true
     //   });
+	/*=============== закрыть  .modal-block по клику на .close-btn ================*/
+	const modalBlocks = document.querySelectorAll('.modal-block');
+	if(modalBlocks.lemgth > 0){
+		for(item of modalBlocks){
+			const thisCloseBtn = item.querySelector('.close-btn');
+			thisCloseBtn.addEventListener('click', function(){
+				item.classList.remove('visible');
+			});
+		}
+	}
+	
 	/*====== PASSWORD VISIBLE/HIDE=============*/
 	document.querySelectorAll(".toggle-pass").forEach(el=>{
 		const tglBtn = el.querySelector(".input-icon__img");
@@ -96,4 +106,14 @@ document.addEventListener("DOMContentLoaded", function (){
 		});
 	});
 
+    /*====== Панель поиска в шапке показать / скрыть =============*/
+	const openHeaderSearchPanel = document.querySelector('#btn-search-panel');
+	const headerSearchPanel = document.querySelector('#header-search-panel');
+	if(openHeaderSearchPanel){
+		openHeaderSearchPanel.addEventListener('click',function(e){
+			e.preventDefault();
+			headerSearchPanel.classList.add('visible');
+		});
+		
+	}
 });
