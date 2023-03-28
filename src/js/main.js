1da54cb78ce8f2ab996c394cada3e6725ca64785
Lareta064 <lareta064@gmail.com>
2023-03-28 13:59:12 +0300

    program tab title

--- src/js/main.js
+++ src/js/main.js
@@ -65,6 +65,20 @@ document.addEventListener("DOMContentLoaded", function (){
 		this.classList.remove('visible');
 	});
    }
+
+   /*================ ТАБЫ - TABS============== */
+   if($('.tabs-wrapper')){
+		$('.tabs-wrapper').each(function() {
+			let ths = $(this);
+			ths.find('.tab-item').not(':first').hide();
+			ths.find('.tab').click(function() {
+				ths.find('.tab').removeClass('active').eq($(this).index()).addClass('active');
+				ths.find('.tab-item').hide().eq($(this).index()).fadeIn()
+			}).eq(0).addClass('active');
+		});
+
+   }
+
 	// //========большой сладер документов =======//
 	// let docSlider = new Swiper(".doc-slider", {
     //    slidesPerView: 1,
